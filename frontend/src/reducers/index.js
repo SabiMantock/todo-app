639fa7c20b314c8d65e7033c17e1074ce6ed083e
SabiMantock <sabimantock@gmail.com>
2022-03-26 23:28:48 +0000

    removed utils

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { loadState } from '../utils/storage'
+
 import todoSlice from './todo'
 
 const store = configureStore({
   reducer: { todo: todoSlice },
-  preloadedState: loadState()
+
 })
 
 export default store
